30673a324fd339bc6638e41328cdae6b09452d0f
Clarky <clarky117@outlook.com>
2022-06-18 13:57:26 +0800

    pseudo code written

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+    // plan
+    
+    // when i visit the page
+
+    // put clock in header
+    
+    // see timeblock 9am to 5pm
+    // should see what i already have in local storage
+
+    // if timeblock is future, it has future class from css
+    // if timeblock is past, it has past class from css
+    // if timeblock is present, it has present class from css
+
+
+    {/* <div class="row">
+    
+          <div class="time-col col-2">9:00</div>
+    
+          <div class="textarea-col col-8">
+            <textarea name="" id="" rows="3"></textarea>
+          </div>
+    
+          <div class="button-col col-2">
+            <button class="btn btn-primary">Save</button>
+          </div>
+    
+        </div > */}
+
+        // when i click on the save button
+        // grab user input
+        // save to local storage
+        
